b08c65804e2f10afddd28fb6ad56a3bdabfa919a
Orne <killu@gmail.com>
2025-10-24 18:21:35 -0300

    modificando ProductManager

---
+++ src/routes/carts.router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import CartManager from "../managers/CartManager";
+
+const router = Router();
+const cartManager = new CartManager("./src/data/carts.json");
+
+router.post("/", async (req, res) => {
+  const newCart = await cartManager.createCart();
+  res.status(201).json(newCart);
+});
