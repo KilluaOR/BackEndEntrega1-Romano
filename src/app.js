8f56c6318512b04238701a29884aa46176aa37e9
Orne <killu@gmail.com>
2025-10-25 19:19:57 -0300

    tratando de correrlo en postman

--- src/app.js
+++ src/app.js
@@ -7,12 +7,15 @@ const PORT = 8080;
 
 // Middleware para manejar JSON.
 app.use(express.json()); //Interpreta datos enviados en fromato JSON.
-app.use(express.urlencoded({ extend: true })); //Interperta datos enviados por formularios.
+app.use(express.urlencoded({ extended: true })); //Interperta datos enviados por formularios.
 
 // Rutas principales
-app.use("/api/products", productosRouter);
+app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
+app.on("request", () => {
+  console.log("Solicitud recibida");
+});
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
--- src/managers/CartManager.js
+++ src/managers/CartManager.js
@@ -20,3 +20,4 @@ class CartManager {
     return newCart;
   }
 }
+export default CartManager;
