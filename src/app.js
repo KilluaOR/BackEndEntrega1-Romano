a126c72161c50a2c4bc24db3f308be384f96cf4f
Orne <killu@gmail.com>
2025-11-10 16:59:10 -0300

    agrego handlebars

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import handlebars from "express-handlebars";
+import __dirname from "path";
 
 const app = express();
 const PORT = 8080;
@@ -12,6 +14,19 @@ app.use(express.urlencoded({ extended: true })); //Interperta datos enviados por
 // Rutas principales
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("views engine", "handlebars");
+app.use(express.static(__dirname + "/views"));
+
+app.get("/", (req, res) => {
+  let testUser = {
+    name: "Coder",
+    last_name: "House",
+  };
+  res.render("index", testUser);
+});
 
 app.on("request", () => {
   console.log("Solicitud recibida");
