4fe2c05163de3527c0bd09d09e17db75ee3704dd
Orne <killu@gmail.com>
2025-11-12 16:50:34 -0300

    agrego metodo put/:pid

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -26,8 +26,36 @@ router.get("/:pid", async (req, res) => {
 });
 
 router.put("/:pid", async (req, res) => {
-  const { pid } = req.params;
-  const newProduct = req.body;
+  try {
+    const { pid } = req.params;
+    const updatedFields = req.body;
+
+    //Validación básica
+    if (!updatedFields || Object.keys(updatedFields).length === 0) {
+      return res
+        .status(400)
+        .json({ error: "No se recibieron campos para actualizar" });
+    }
+
+    //Convierto el pid a num si los ids  son numéricos en el JSON.
+    const idNum = isNaN(pid) ? pid : parseInt(pid);
+
+    const result = await productManager.updateProduct(idNum, updatedFields);
+
+    if (result && result.error) {
+      return res.status(404).json(result);
+    }
+
+    return req.json({
+      message: "Producto actualizado correctamente",
+      product: result,
+    });
+  } catch (error) {
+    console.error("PUT /:pid error:", error);
+    return res
+      .status(500)
+      .json({ error: "Error interno al actualizar producto" });
+  }
 });
 
 export default router;
