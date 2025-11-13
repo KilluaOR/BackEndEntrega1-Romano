5cd7c16461640a2a3a71a13e5e8d18883daca8dc
Orne <killu@gmail.com>
2025-11-13 17:55:29 -0300

    fixing controllers

--- src/app.js
+++ src/app.js
@@ -3,31 +3,27 @@ import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
 import handlebars from "express-handlebars";
 import __dirname from "path";
+import viewsRouter from "./routes/products.router.js";
 
 const app = express();
 const PORT = 8080;
-
-// Middleware para manejar JSON.
-app.use(express.json()); //Interpreta datos enviados en fromato JSON.
-app.use(express.urlencoded({ extended: true })); //Interperta datos enviados por formularios.
 
 // Rutas principales
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
+//SOCKET.IO
+// export const io = new Server
+
 //Handlebars
 app.engine("handlebars", handlebars.engine());
-app.set("views", __dirname + "/views");
 app.set("views engine", "handlebars");
+app.set("views", __dirname + "./src/views");
 app.use(express.static(__dirname + "/views"));
 
-// app.get("/", (req, res) => {
-//   let testUser = {
-//     name: "Coder",
-//     last_name: "House",
-//   };
-//   res.render("index", testUser);
-// });
+// Middleware para manejar JSON.
+app.use(express.json()); //Interpreta datos enviados en fromato JSON.
+app.use(express.urlencoded({ extended: true })); //Interperta datos enviados por formularios.
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
