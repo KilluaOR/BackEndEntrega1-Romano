3bc878f9119ec3aa2ab09751d7cce3d2ce6f464f
Orne <killu@gmail.com>
2025-12-05 19:04:06 -0300

    fixing mistakes

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -2,24 +2,27 @@ import mongoose from "mongoose";
 
 const cartCollection = "carts";
 
-const cartSchema = new mongoose.Schema({
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "products", // Referencia al modelo Product (mongoose pluraliza automáticamente)
-        required: true,
+const cartSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "products",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+          min: [1, "La cantidad debe ser al menos 1"],
+        },
       },
-      quantity: {
-        type: Number,
-        default: 1,
-        min: [1, "La cantidad debe ser al menos 1"],
-      },
-    },
-  ],
-}, {
-  timestamps: true, // Agregar createdAt y updatedAt automáticamente
-});
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const CartModel = mongoose.model(cartCollection, cartSchema);
 
